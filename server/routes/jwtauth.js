048075191c668dee6adfeff7ab5a061938a8dc9b
joshfagen <j.fagen@muxlab.com>
2020-04-30 02:13:36 -0400

    registration implemented, new user added to db

--- server/routes/jwtauth.js
+++ server/routes/jwtauth.js
@@ -1,31 +1,41 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 const pool = require('../db.js');   
-
+const bcrypt = require('bcrypt');
 
 
 router.post("/register", async (req, res) => {
 
     try {
          // Take apart req.body (name, email, pass)
-            const { user_name, user_email, pass } = req.body;
+            const { name, email, password } = req.body;
            
         // Check if email already exists (if so, throw error)
             const user = await pool.query("SELECT * FROM users WHERE user_name = $1", [
-                user_name
+                name
             ]);
 
             if (user.rows.length > 0) {
                 return res.status(401).json("That username was already taken!");
-              } else {
-                  return res.status(200).json('username not taken!');
-              }
+              } 
+              
 
+              
         // Bcrypt password
+              
+            const saltRound = 10;
+            const salt = await bcrypt.genSalt(saltRound);
+            
+            const bcryptPassword = await bcrypt.hash(password, salt);
 
         // Insert details in db
-
-        // Generate JWT 
+            const newUser = await pool.query("INSERT INTO USERS(user_name, user_email, user_password) VALUES($1, $2, $3) RETURNING *", [
+                name, email, bcryptPassword
+            ]);
+            
+            res.json(newUser.rows[0]);
+        
+            // Generate JWT 
         
     } catch (err) {
         console.log(err.message);
